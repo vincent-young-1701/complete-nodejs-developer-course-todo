da2f807d5f9f844a163c1888fed1a28d149d9926
Vincent Young <vincent_young@apple.com>
2018-08-07 21:23:34 -0700

    First commit

---
+++ playground/mongodb-find.js
@@ -0,0 +1,29 @@
+const {MongoClient, ObjectID} = require('mongodb');
+
+MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, client) => {
+    if(err) {
+        return console.log('Unable to connect to MongoDB');
+    }
+    console.log('Connected to MongoDB server');
+
+    const db = client.db('TodoApp');
+
+    // db.collection('Todos').find({
+    //     _id: new ObjectID('5b6a6da618a3b952dbf1e635')
+    // }).toArray().then((docs)=> {
+    //     console.log('Todos');
+    //     console.log(JSON.stringify(docs, undefined, 2));
+    // }, (err) => {
+    //     console.log('Unable to fetch todos', err);
+    // });
+
+    db.collection('Todos').find().count().then((docs)=> {
+        console.log('Todos');
+        console.log(JSON.stringify(docs, undefined, 2));
+    }, (err) => {
+        console.log('Unable to fetch todos', err);
+    });    
+
+
+    client.close();
+});
