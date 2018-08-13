bf51a1b1125288ac96db527874aa41a21a41fc41
Vincent Young <vincent_young@apple.com>
2018-08-13 11:59:24 -0700

    Added get users/me

--- server/server.js
+++ server/server.js
@@ -7,6 +7,7 @@ var bodyParser = require('body-parser');
 var {mongoose} = require('./db/mongoose');
 var {Todo} = require('./models/todo');
 var {User} = require('./models/user');
+var {authenticate} = require('./middleware/authenticate');
 
 var app = express();
 
@@ -125,6 +126,10 @@ app.post('/users', (req, res) => {
     }).catch((e) => {
         res.status(400).send(e);
     });
+});
+
+app.get('/users/me', authenticate, (req, res) => {
+    res.status(200).send(req.user);
 });
 
 app.listen(port, () => {
