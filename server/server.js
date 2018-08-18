41974e1313ca024765ce1fcbfbf938fa39176a8c
Vincent Young <vincent_young@apple.com>
2018-08-18 13:59:59 -0700

    Added login route

--- server/server.js
+++ server/server.js
@@ -132,6 +132,21 @@ app.get('/users/me', authenticate, (req, res) => {
     res.status(200).send(req.user);
 });
 
+// POST /users/login {email, password}
+
+app.post('/users/login', (req, res) => {
+    
+    var body = _.pick(req.body, ['email', 'password']);
+
+    User.findByCredentials(body.email, body.password).then((user) => {
+        user.generateAuthToken().then((token) => {
+            res.header('x-auth', token).send(user);
+        });
+    }).catch((e) => {
+        res.status(400).send();
+    });
+});
+
 app.listen(port, () => {
     console.log(`Started on port ${port}`);
 });
